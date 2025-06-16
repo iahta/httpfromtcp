8656ede2f14b90dd5b3b70e09f401cfa7c7bfcad
iahta <153889016+iahta@users.noreply.github.com>
2025-06-16 13:52:56 -0500

    read by 8 bytes

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const inputFilePath = "messages.txt"
+
+func main() {
+	file, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
+	}
+	defer file.Close()
+
+	for {
+		b := make([]byte, 8)
+		n, err := file.Read(b)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Printf("error: %s\n", err.Error())
+			break
+		}
+		bString := string(b[:n])
+		fmt.Printf("read: %s\n", bString)
+	}
+
+}
